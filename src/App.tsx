0171125fa3fd60b8c70b9478db7f6969fa2a0c4d
makoto <d.makoto0701@yahoo.com>
2023-09-28 00:13:41 -0700

    configure usedisAppselector useAppDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from "react";
+// import { useSelector } from 'react-redux'
+import {useAppSelector} from "./app/hooks";
 import "./App.scss";
-import Sidebar from "./components/sidebar/Sidebar"
-import Chat from "./components/chat/Chat"
+import Sidebar from "./components/sidebar/Sidebar";
+import Chat from "./components/chat/Chat";
+import Login from "./components/login/Login"
 
 function App() {
-  return <div className="App">
-    {/* sidebar */}
-    <Sidebar />
-    {/* chat */}
-    <Chat />
-    </div>;
+  const user = useAppSelector((state) => state.user);
+// const user = null
+  return (
+    <div className="App">
+      {user ? (
+        <>
+          {/* sidebar */}
+          <Sidebar />
+          {/* chat */}
+          <Chat />
+        </>
+      ) : (
+        <><Login /></>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/app/hooks.ts
@@ -0,0 +1,5 @@
+import { AppDispatch, RootState } from "./store";
+import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
